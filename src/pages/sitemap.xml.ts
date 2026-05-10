7a358fc8006cfd3548fa8c3a94f9ea4824209598
Márcio Lucas R Oliveira <marcio.oliveira@emiteai.com.br>
2026-05-10 11:44:34 -0300

    feat: serve sitemap at /sitemap.xml (standard path) for search console

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,14 @@
+import type { APIRoute } from 'astro';
+
+export const GET: APIRoute = async ({ site }) => {
+  const base = site?.toString().replace(/\/$/, '') ?? 'https://marciioluucas.github.io';
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>${base}/sitemap-0.xml</loc>
+  </sitemap>
+</sitemapindex>`;
+  return new Response(xml, {
+    headers: { 'Content-Type': 'application/xml; charset=utf-8' },
+  });
+};
